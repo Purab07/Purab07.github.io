d90e7bf5e5d6dbb2b72dcfe37b003ae70402a9f9
Purab07 <73265862+Purab07@users.noreply.github.com>
2022-07-01 15:06:28 -0800

    Update design.js

--- design.js
+++ design.js
@@ -1,5 +1,12 @@
-var canvas = createCanvas(1000,1000);
-var ctx;
-ctx.fillStyle = "green";
-ctx.rect(100,100,100,100);
-ctx.fill();
+function setup()
+{
+  var ctx;
+  ctx.fillStyle = "green";
+  ctx.rect(100,100,100,100);
+  ctx.fill();
+}
+
+function draw()
+{
+  var canvas = createCanvas(1000,1000);
+}
